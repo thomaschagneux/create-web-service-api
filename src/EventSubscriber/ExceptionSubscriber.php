62fe3b4eaf59f16708cd4139b6bb90e186ebad24
Thomas Chagneux <Thomas.chagneux@gmail.com>
2025-06-02 11:30:48 +0200

    P7-2 add exceptions managing

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -3,13 +3,29 @@
 namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
     public function onExceptionEvent(ExceptionEvent $event): void
     {
-        // ...
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof HttpException) {
+            $data = [
+                'status' => $exception->getStatusCode(),
+                'message' => $exception->getMessage(),
+            ];
+        } else {
+            $data = [
+              'status' => 500,
+              'message' => $exception->getMessage(),
+            ];
+        }
+
+        $event->setResponse(new JsonResponse($data));
     }
 
     public static function getSubscribedEvents(): array
